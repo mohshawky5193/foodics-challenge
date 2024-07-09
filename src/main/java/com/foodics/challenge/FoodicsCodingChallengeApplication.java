cdd232cc2a58197570b41597d05dfc1d941c48c9
Mohamed Shawky <mam2210_@hotmail.com>
2024-07-09 22:05:54 +0300

    Initial Spring Project for Foodics Challenge

---
+++ src/main/java/com/foodics/challenge/FoodicsCodingChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.foodics.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodicsCodingChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodicsCodingChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/foodics/challenge/FoodicsCodingChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodics.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodicsCodingChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
